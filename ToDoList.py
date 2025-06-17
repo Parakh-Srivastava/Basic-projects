32b51905e0e06c55f79893ea3c6228aec300f8cc
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-06-18 01:14:15 +0530

    Project 1

--- ToDoList.py
+++ ToDoList.py
@@ -1,11 +1,11 @@
 List = []
-print(List)
 goback = 1
 while goback == 1:
     inp = int(input("If you wanna add please type 1.\nIf you wanna Edit press 2.\nIf you wanna delete press 3."))
     if(inp == 1):
         add = input("Enter what do you want to note :")
         List.append(add)
+        print(List)
     elif(inp == 2):    
         print("Which element do you want to edit ? ( 1 to", len(List), ").")
         edit = int(input())
@@ -19,4 +19,4 @@ while goback == 1:
         print(List)
     else:
         print("Enter the values which are given in the option !!!")
-    goback = int(input("Do you want to make another change? (yes = 1 ; no = 2)"))
+    goback = int(input("Do you want to make another change? (yes = 1 ; no = Any other 'logical' button)"))
