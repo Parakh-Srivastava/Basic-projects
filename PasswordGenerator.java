a24bbf106554f11d02eb78e25519b6ddb7b81916
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-06-21 16:51:37 +0530

    project updates

--- PasswordGenerator.java
+++ PasswordGenerator.java
@@ -7,11 +7,60 @@ public class PasswordGenerator
         Random ra = new Random();
         System.out.print("Enter the length of the password:");
         int pa = sc.nextInt();
-        char[] index = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
-        for(int i = 1; i <= pa;i++)
+        char[] index = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+        char[] number = {'1','2','3','4','5','6','7','8','9','0',};
+        char[] signs = {'!','@','#','$','%','&','?','/'};
+        System.out.println("Write the option : \n1 - Only Letters \n2 - Letters and numbers \n3 - nums, letters and sign");
+        int opt = sc.nextInt();
+        if(opt == 1)
         {
-            int r = ra.nextInt(index.length);
-            System.out.print(index[r]);
+            System.out.print(index[ra.nextInt(index.length)]);
+            for(int i = 2; i <= pa;i++)
+            {
+                int chr = ra.nextInt(index.length);
+                System.out.print(index[chr]);
+            }
+        }
+        else if(opt == 2)
+        {
+            System.out.print(index[ra.nextInt(index.length)]);
+            for(int i = 2; i <= pa;i++)
+            {
+                int ArraySelector = ra.nextInt(1,3); 
+                if(ArraySelector == 1)
+                {
+                    int chr = ra.nextInt(number.length);
+                    System.out.print(number[chr]);
+                }
+                else if(ArraySelector == 2)
+                {
+                    int chr = ra.nextInt(index.length);
+                    System.out.print(index[chr]);
+                }
+            }
+        }
+        else if(opt == 3)
+        {
+            System.out.print(index[ra.nextInt(index.length)]);
+            for(int i = 2; i <= pa;i++)
+            {
+                int ArraySelector = ra.nextInt(1,4); 
+                if(ArraySelector == 1)
+                {
+                    int chr = ra.nextInt(number.length);
+                    System.out.print(number[chr]);
+                }
+                else if(ArraySelector == 2)
+                {
+                    int chr = ra.nextInt(index.length);
+                    System.out.print(index[chr]);
+                }
+                else if(ArraySelector == 3)
+                {
+                    int chr = ra.nextInt(signs.length);
+                    System.out.print(signs[chr]);
+                }
+            }
         }
         sc.close();
         
---
+++ test.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class test 
+{
+    public static void main(String[] args) 
+    {
+        Random ra = new Random();
+        int a = 1;
+        a = ra.nextInt();
+        System.out.print(a);
+    }
+}
