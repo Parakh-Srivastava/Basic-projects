a5ddedabd9b7221c6e06681193ad974a18607394
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-06-26 23:20:30 +0530

    tic-Tac-toe

--- TTT.py
+++ TTT.py
@@ -1,25 +1,64 @@
+import random
 gameboard = [[1,'|',2,'|',3],['-','+','-','+','-'],[4,'|',5,'|',6],['-','+','-','+','-'],[7,'|',8,'|',9]]
 def gb():
     for i in range(0,5):
         for j in range(0,5):
             print(gameboard[i][j], end=' ')
         print()
-def X_input():
-    X = int(input("X's turn :"))
-    while (1 > X or X > 9):
-        X = int(input("Enter numbers from 1 to 9 buddy :"))
-    for i in range(0,5):
-        for j in range(0,5):
-            if(gameboard[i][j] == X):
-                gameboard[i][j] = 'X'
+def X_inputP():
+    ch = True
+    while(ch == True):
+        X = int(input("X's turn :"))
+        while (1 > X or X > 9):
+            X = int(input("Enter numbers from 1 to 9 buddy :"))
+        for i in range(0,5):
+            for j in range(0,5):
+                if(gameboard[i][j] == X ):
+                    break
+            if(gameboard[i][j] == X ):
+                break
+        if(gameboard[i][j] == X):
+            gameboard[i][j] = 'X'
+            ch = False
+        else:
+            print("invalid output !")
+            ch = True
+def X_inputC():
+    ch = True
+    while(ch == True):
+        X = random.randint(1,9)
+        while (1 > X or X > 9):
+            X = int(input("Enter numbers from 1 to 9 buddy :"))
+        for i in range(0,5):
+            for j in range(0,5):
+                if(gameboard[i][j] == X ):
+                    break
+            if(gameboard[i][j] == X ):
+                break
+        if(gameboard[i][j] == X):
+            gameboard[i][j] = 'X'
+            print(f"X = {X}")
+            ch = False
+        else:
+            ch = True
 def O_input():
-    O = int(input("O's turn :"))
-    while (1 > O or O > 9):
-        O = int(input("Enter numbers from 1 to 9 buddy :"))
-    for i in range(0,5):
-        for j in range(0,5):
-            if(gameboard[i][j] == O):
-                gameboard[i][j] = 'O'
+    ch = True
+    while(ch == True):
+        O = int(input("O's turn :"))
+        while (1 > O or O > 9):
+            O = int(input("Enter numbers from 1 to 9 buddy :"))
+        for i in range(0,5):
+            for j in range(0,5):
+                if(gameboard[i][j] == O ):
+                    break
+            if(gameboard[i][j] == O ):
+                break
+        if(gameboard[i][j] == O):
+            gameboard[i][j] = 'O'
+            ch = False
+        else:
+            print("invalid output !")
+            ch = True
 def gameEndX():
     if(gameboard[0][0] == 'X' and gameboard[0][2] == 'X' and gameboard[0][4] == 'X'):
         print("X wins !!")
@@ -79,10 +118,14 @@ def gameDraw():
         if(gameEndO() == False and gameEndX() == False):
             print("The game is a draw !!")
             return True
+CP = int(input("PvP = 1 || PvC = 2 :"))
 print('Write the number where you want the respective symbol')
 gb()
 while (True):
-    X_input()
+    if(CP == 1):
+        X_inputP()
+    elif(CP == 2):
+        X_inputC()
     gb()
     if(gameEndX() == True):
         break
