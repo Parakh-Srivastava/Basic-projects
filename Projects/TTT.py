dd14e18ebe69af3c21f4353519da70eb01e1b3bd
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-07-02 21:40:24 +0530

    Tic Tac Toe

--- Projects/TTT.py
+++ Projects/TTT.py
@@ -61,62 +61,36 @@ def O_input():
             print("invalid output !")
             ch = True
 def gameEndX():
-    if(gameboard[0][0] == 'X' and gameboard[0][2] == 'X' and gameboard[0][4] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[2][0] == 'X' and gameboard[2][2] == 'X' and gameboard[2][4] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[4][0] == 'X' and gameboard[4][2] == 'X' and gameboard[4][4] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[0][0] == 'X' and gameboard[2][0] == 'X' and gameboard[4][0] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[0][2] == 'X' and gameboard[2][2] == 'X' and gameboard[4][2] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[0][4] == 'X' and gameboard[2][4] == 'X' and gameboard[4][4] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[0][0] == 'X' and gameboard[2][2] == 'X' and gameboard[4][4] == 'X'):
-        print("X wins !!")
-        return True
-    elif(gameboard[0][4] == 'X' and gameboard[2][2] == 'X' and gameboard[4][0] == 'X'):
+    if((gameboard[0][0] == 'X' and gameboard[0][2] == 'X' and gameboard[0][4] == 'X')
+       or (gameboard[2][0] == 'X' and gameboard[2][2] == 'X' and gameboard[2][4] == 'X')
+       or (gameboard[4][0] == 'X' and gameboard[4][2] == 'X' and gameboard[4][4] == 'X')
+       or (gameboard[0][0] == 'X' and gameboard[2][0] == 'X' and gameboard[4][0] == 'X')
+       or (gameboard[0][2] == 'X' and gameboard[2][2] == 'X' and gameboard[4][2] == 'X')
+       or (gameboard[0][4] == 'X' and gameboard[2][4] == 'X' and gameboard[4][4] == 'X')
+       or (gameboard[0][0] == 'X' and gameboard[2][2] == 'X' and gameboard[4][4] == 'X') 
+       or (gameboard[0][4] == 'X' and gameboard[2][2] == 'X' and gameboard[4][0] == 'X')):
         print("X wins !!")
         return True
     else:
         return False
 def gameEndO():
-        if(gameboard[0][0] == 'O' and gameboard[0][2] == 'O' and gameboard[0][4] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[2][0] == 'O' and gameboard[2][2] == 'O' and gameboard[2][4] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[4][0] == 'O' and gameboard[4][2] == 'O' and gameboard[4][4] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[0][0] == 'O' and gameboard[2][0] == 'O' and gameboard[4][0] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[0][2] == 'O' and gameboard[2][2] == 'O' and gameboard[4][2] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[0][4] == 'O' and gameboard[2][4] == 'O' and gameboard[4][4] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[0][0] == 'O' and gameboard[2][2] == 'O' and gameboard[4][4] == 'O'):
-            print("O wins !!")
-            return True
-        elif(gameboard[0][4] == 'O' and gameboard[2][2] == 'O' and gameboard[4][0] == 'O'):
-            print("O wins !!")
-            return True
-        else:
-            return False
+    if((gameboard[0][0] == 'O' and gameboard[0][2] == 'O' and gameboard[0][4] == 'O')
+        or (gameboard[2][0] == 'O' and gameboard[2][2] == 'O' and gameboard[2][4] == 'O')
+        or (gameboard[4][0] == 'O' and gameboard[4][2] == 'O' and gameboard[4][4] == 'O')
+        or (gameboard[0][0] == 'O' and gameboard[2][0] == 'O' and gameboard[4][0] == 'O')
+        or (gameboard[0][2] == 'O' and gameboard[2][2] == 'O' and gameboard[4][2] == 'O')
+        or (gameboard[0][4] == 'O' and gameboard[2][4] == 'O' and gameboard[4][4] == 'O')
+        or (gameboard[0][0] == 'O' and gameboard[2][2] == 'O' and gameboard[4][4] == 'O')
+        or (gameboard[0][4] == 'O' and gameboard[2][2] == 'O' and gameboard[4][0] == 'O')):
+        print("O wins !!")
+        return True
+    else:
+        return False
 def gameDraw():
-    if(gameboard[0][0] != 1 and gameboard[0][2] != 2 and gameboard[0][4] != 3 and gameboard[2][0] != 4 and gameboard[2][2] != 5 and gameboard[2][4] != 6 and gameboard[4][0] != 7 and gameboard[4][2] != 8 and gameboard[4][4] != 9):
-        if(gameEndO() == False and gameEndX() == False):
+    if(gameboard[0][0] != 1 and gameboard[0][2] != 2 and gameboard[0][4] != 3
+       and gameboard[2][0] != 4 and gameboard[2][2] != 5 and gameboard[2][4] != 6
+       and gameboard[4][0] != 7 and gameboard[4][2] != 8 and gameboard[4][4] != 9):
+        if((gameEndO() == False) and (gameEndX() == False)):
             print("The game is a draw !!")
             return True
 CP = int(input("PvP = 1 || PvC = 2 :"))
