2418136ca543930fa4933a4ce6026c47625f3c1f
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-07-06 20:11:24 +0530

    Cafetaria

--- Projects/cafepor.py
+++ Projects/cafepor.py
@@ -5,13 +5,20 @@ def menu():
         print(f"{i + 1}. {Items[i]} = ${prices[i]}.","\t")
     print(f"{(len(Items) + 1)}. Exit.")
 menu()
+Bill = []
 totalBill = 0
 inp = int(input("Enter the S.no to select the option :"))
 while(inp != 6):
     if(inp < 7 and inp > 0):
         quantity = int(input(f"How many of {Items[inp - 1]}s :"))
         totalBill += (quantity * prices[inp - 1])
+        localBill = (quantity * prices[inp - 1])
+        Bill.append([Items[inp - 1], quantity, localBill])
+        localBill = 0
     else:
         print("Enter the the numbers from the menu !!")
     inp = int(input("\nAny thing else :"))
-print(f"Your total bill = ${totalBill}!")
+print("")
+for i in range(0,len(Bill)):
+    print(f"{Bill[i][0]} x{Bill[i][1]} = ${Bill[i][2]}")
+print(f"\nYour total bill = ${totalBill}!")
