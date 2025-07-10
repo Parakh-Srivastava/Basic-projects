da148e580119c6917db244a1a8425f9fd8f4378c
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-07-11 00:33:33 +0530

    Pass words

--- Projects/PasswordGenerator.java
+++ Projects/PasswordGenerator.java
@@ -8,7 +8,7 @@ public class PasswordGenerator
         System.out.print("Enter the length of the password:");
         int pa = sc.nextInt();
         char[] index = {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
-        char[] number = {'1','2','3','4','5','6','7','8','9','0',};
+        char[] number = {'1','2','3','4','5','6','7','8','9','0'};
         char[] signs = {'!','@','#','$','%','&','?','/'};
         System.out.print(index[ra.nextInt(index.length)]);
         for(int i = 2; i <= pa;i++)
