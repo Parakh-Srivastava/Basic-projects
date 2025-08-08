120d679fb58cddf3fff17f98d0e2646c0f763349
Parakh-Srivastava <parakhsriv01@gmail.com>
2025-08-09 02:01:58 +0530

    Error handeling!!

--- Projects/Games/ticTacToe.py
+++ Projects/Games/ticTacToe.py
@@ -11,17 +11,17 @@ def inputC(XO):
 
     inp = random.randint(1,9)
     
-    while (1 > inp or inp > 9):
+    while (1 > inp) or (inp > 9):
         inp = int(input("Enter numbers from 1 to 9 buddy :")) #Value under 1 to 9 will be selected
 
     for i in range(0,5):
         for j in range(0,5):
-            if(gameboard[i][j] == inp):
+            if gameboard[i][j] == inp:
                 break
-        if(gameboard[i][j] == inp):
+        if gameboard[i][j] == inp:
             break
 
-    if(gameboard[i][j] == inp):
+    if gameboard[i][j] == inp:
         gameboard[i][j] = XO
         print(f"{XO} plays {inp}.")    
     else:
@@ -31,17 +31,17 @@ def input_Value(XO):
 
     inp = int(input(f"{XO}'s turn :"))
     
-    while (1 > inp or inp > 9): 
+    while (1 > inp) or (inp > 9): 
         inp = int(input("Enter numbers from 1 to 9 buddy :"))   #Value under 1 to 9 will be selected
         
     for i in range(0,5):
         for j in range(0,5):
-            if(gameboard[i][j] == inp):
+            if gameboard[i][j] == inp :
                 break
-        if(gameboard[i][j] == inp):
+        if gameboard[i][j] == inp:
             break
         
-    if(gameboard[i][j] == inp):        #So that we can see if the entered number is not already filled.
+    if gameboard[i][j] == inp:        #So that we can see if the entered number is not already filled.
         gameboard[i][j] = XO
     else:
         print("invalid output !")
@@ -71,48 +71,63 @@ def gameDraw():
         return False
 
 def main():
-    print("Write the number where you want the respective symbol !!")
-    gb()
-    CorP = int(input("[Enter 1 for PvP] | [Enter 2 for PvC] : "))
-    if CorP == 2:
-        XorO = int(input("[Enter 1 to select O] [Enter 2 to select X] : \n"))
-    while (True):
-        
-        if CorP == 1:
-            input_Value('X')
-        elif CorP == 2:
-            if XorO == 1:
-                inputC('X')
-            elif XorO == 2:
+    
+    try:
+        print("Write the number where you want the respective symbol !!")
+        gb()
+        CorP = int(input("[Enter 1 for PvP] | [Enter 2 for PvC] : "))
+        if CorP == 2:
+            XorO = int(input("[Enter 1 to select O] [Enter 2 to select X] : \n"))
+        while True:
+            
+            if CorP == 1:
                 input_Value('X')
+            elif CorP == 2:
+                if XorO == 1:
+                    inputC('X')
+                elif XorO == 2:
+                    input_Value('X')
+                else :
+                    print("Either 1 or 2, game closed due to retardness.")
+                    break
             else :
                 print("Either 1 or 2, game closed due to retardness.")
                 break
-        else :
-            print("Either 1 or 2, game closed due to retardness.")
-            break
-        print("\n")
-        
-        gb()
-        if(gameEnd('X') == True):
-            break
-        elif(gameDraw() == True):      
-            break
-        
-        if CorP == 1:
-            input_Value('O')
-        elif CorP == 2:
-            if XorO == 1:
+            print("\n")
+            
+            gb()
+            if gameEnd('X') == True:
+                break
+            elif gameDraw() == True:      
+                break
+            
+            if CorP == 1:
                 input_Value('O')
-            elif XorO == 2:
-                inputC('O')
-        print("\n")
+            elif CorP == 2:
+                if XorO == 1:
+                    input_Value('O')
+                elif XorO == 2:
+                    inputC('O')
+            print("\n")
 
-        gb()
-        if(gameEnd('O') == True):
-            break
-        elif(gameDraw() == True):      
-            break
+            gb()
+            if gameEnd('O') == True:
+                break
+            elif gameDraw() == True:      
+                break
+    
+    except ValueError:
+        print("Input had to be a number\n")
+    
+    finally:
+        playAgain = input("Do you wanna play again ! :")
+
+        if playAgain[0] == "y" or playAgain[0] == "Y":
+            main()
+        elif playAgain[0] == "n" or playAgain == "N":
+            print("Exiting game !")
+        else:
+            print("Not a yes or a no results in the closing of the game !!")
 
 if __name__ == "__main__":
     main()
