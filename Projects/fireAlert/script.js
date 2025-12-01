cef8f663a247bf9d1d4ccbec424bdf82a991a6d0
Parakh Srivastava <Parakh Srivastava@parakhsriv01@gmail.com>
2025-12-01 23:54:21 +0530

    moved file

---
+++ Projects/fireAlert/script.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("mkbag");
+            
+function playAudio()
+{        
+    x.play();
+}
+
+function gothere()
+{
+    setTimeout(() => {window.location.href= "https://youtube.com/shorts/-y_DvI8HwEU?si=zP-8vpLh6Zmp2yvS";}, 2300)
+}
